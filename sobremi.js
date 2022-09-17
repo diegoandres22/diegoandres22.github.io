42bcca8f3f5509a803b0ce7fe6640722b83646f3
diegoandres22 <89799715+diegoandres22@users.noreply.github.com>
2022-09-17 18:40:29 -0300

    Add files via upload

--- sobremi.js
+++ sobremi.js
@@ -1,29 +1,18 @@
-const conteneLoadin= document.querySelector(".conteneLoadin");
+const rayas = document.getElementById("icono-nav");
+const nav = document.getElementById("nav");
 
-
-window.setInterval(() => {
-    conteneLoadin.style.opacity='0';
-    conteneLoadin.style.visibility='hidden';
-    conteneLoadin.style.zIndex='0';
-    
-}, 2000);
-
-
-
-
-
-
-
-
-
-
-
-function responsiveMenu(){
-    var x = document.getElementById( "nav");
+rayas.addEventListener("click", () =>{
    
-    var titu=document.querySelectorAll(".titu");
-   x.classList.toggle("responsive");
-   for(let ti of titu){
-    ti.classList.toggle("invisible");
-   }
-}
+   nav.classList.toggle("responsive")
+})
+
+
+ const conteneLoadin= document.querySelector(".conteneLoadin");
+ window.setInterval(() => {
+     conteneLoadin.style.opacity='0';
+     conteneLoadin.style.visibility='hidden';
+     conteneLoadin.style.zIndex='0';
+    
+ }, 3000);
+
+
